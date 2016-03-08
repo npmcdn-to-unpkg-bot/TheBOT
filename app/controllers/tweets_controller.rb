da9117163d4cba4a0cd19c3e8350895cda4a16be
LeShoyaStarkey <olstarkey@gmail.com>
2016-03-07 16:54:48 -0800

    corrected .env file

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,17 @@
+
 class TweetsController < ApplicationController
 
   def news
-
+    # PUT THIS IN OWN SEPERATE METHOD CALLED BY DIFFERENT METHODS FOR CONTROLLER
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
   end
+
+
+
 
 end
